9e41c5c52a0d005f7815dcdf058b47f6a54d87ef
snowden <faridibnchouaib89@gmail.com>
2022-03-22 11:51:17 +0100

    Add query

--- src/main/java/com/muguiwara/luffy/demo/repo/UserEntityRepository.java
+++ src/main/java/com/muguiwara/luffy/demo/repo/UserEntityRepository.java
@@ -2,10 +2,12 @@ package com.muguiwara.luffy.demo.repo;
 
 import com.muguiwara.luffy.demo.model.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserEntityRepository extends MongoRepository<UserEntity, String> {
 
-    UserEntity findByUsername(String username);
+    @Query(value = "{username: '?0'}")
+    UserEntity findUserByUsername(String username);
 }
